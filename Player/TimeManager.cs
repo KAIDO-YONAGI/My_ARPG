07d61f33c9319a2cb205179fa7aeb36b463d6cd3
KAIDOYONAGI <1224855487@qq.com>
2026-03-18 19:50:02 +0800

    TimeManager

---
+++ Player/TimeManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    public static TimeManager Instance;
+    public  bool IsGamePaused;
+    private int numberOfPauses = 0;
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+
+    public void PauseGame()//需要调用脚本配合使用bool变量保证只能暂停一次，不能重复调用导致时间缩放异常
+    {
+        Time.timeScale = 0;
+        numberOfPauses++; 
+        IsGamePaused = true;
+
+    }
+
+    public void ResumeGame()
+    {
+        
+        numberOfPauses--;
+        if (numberOfPauses < 0) numberOfPauses = 0;
+        if (numberOfPauses == 0)
+        {
+            Time.timeScale = 1;
+            IsGamePaused = false;
+        }
+    }
+}
