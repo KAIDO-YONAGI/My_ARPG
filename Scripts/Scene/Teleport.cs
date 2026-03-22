10f3539b259e31365f00099d8949455083bea235
KAIDOYONAGI <1224855487@qq.com>
2026-03-22 22:27:12 +0800

    sceneload refactor

---
+++ Scripts/Scene/Teleport.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public SceneLoadEventSO loadEventSO;
+    public Vector3 newPosition;
+    public GameSceneSO sceneToLoad;
+    public bool isToFade = true;
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        loadEventSO.RaiseLoadRequestEvent(sceneToLoad, newPosition, isToFade);
+    }
+}
