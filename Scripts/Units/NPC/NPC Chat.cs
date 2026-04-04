a7452adc5cd718f4688bb6ec6cd66627b3f1f5e6
KAIDOYONAGI <1224855487@qq.com>
2026-04-04 21:56:56 +0800

    NPC Chat ing

---
+++ Scripts/Units/NPC/NPC Chat.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCChat : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+}
