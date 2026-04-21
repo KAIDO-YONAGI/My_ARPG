b550fd88b58c8b3c1a7fac3f95d19f51c9d9df63
KAIDOYONAGI <1224855487@qq.com>
2026-04-21 20:38:01 +0800

    QuestObjective

--- Assets/Scripts/UI/QuestLog/QuestManager.cs
+++ Assets/Scripts/UI/QuestLog/QuestManager.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class QuestManager : MonoBehaviour
@@ -18,13 +20,29 @@ public class QuestManager : MonoBehaviour
         }
     }
     private Dictionary<QuestSO, Dictionary<QuestObjective, int>> questProgress = new();
-    public void UpdateObjectiveProgress(QuestSO quest,QuestObjective obj)
+    public void UpdateObjectiveProgress(QuestSO quest, QuestObjective obj)
     {
-        
+        if (!questProgress.ContainsKey(quest))
+        {
+            questProgress.Add(quest, new Dictionary<QuestObjective, int>());
+        }
+        var progressDictionary = questProgress[quest];
+        int newAmount = 0;
+
+        if (obj.targetItem != null)
+        {
+            newAmount = ItemHistoryManager.instance.GetItemQuantity(obj.targetItem);
+        }
+        else if (obj.targetCharacter != null && ConversationHistoryManager.instance.HasChatedWith(obj.targetCharacter))
+        {
+            newAmount = obj.requiredAmount;
+        }
+
+        progressDictionary[obj] = newAmount;
     }
     public string GetProgressText(QuestSO quest, QuestObjective obj)
     {
-        int currentAmount = 0;
+        int currentAmount = GetCurrentAmount(quest,obj);
 
         if (currentAmount >= obj.requiredAmount)
             return "Completed";
@@ -32,5 +50,12 @@ public class QuestManager : MonoBehaviour
             return $"{currentAmount}/{obj.requiredAmount}";
         else
             return "In Progress";
+    }
+    public int GetCurrentAmount(QuestSO quest, QuestObjective obj)
+    {
+        if (questProgress.TryGetValue(quest, out var objectiveDictionary))
+            if (objectiveDictionary.TryGetValue(obj, out int amount))
+                return amount;
+        return -1;
     }
 }
