76b9411e3a7f15ed112995491f1f4bbbc4acd723
KAIDOYONAGI <1224855487@qq.com>
2026-04-26 23:48:08 +0800

    debug

--- Assets/Scripts/Player/ShiftEquipment.cs
+++ Assets/Scripts/Player/ShiftEquipment.cs
@@ -1,17 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MyEnums;
 
 public class ShiftEquipment : MonoBehaviour
 {
     public PlayerCombat combat;
     public PlayerBow bow;
+    public PlayerMovement playerMovement;
+
+    private float shiftCooldown = 0.3f;
+    private float shiftTimer;
 
     private void Update()
     {
-        if (Input.GetButtonDown("ShiftEquipment")){
-            combat.enabled=!combat.enabled;
-            bow.enabled=!bow.enabled;
+        if (shiftTimer > 0)
+            shiftTimer -= Time.deltaTime;
+
+        if (Input.GetButtonDown("ShiftEquipment") && shiftTimer <= 0)
+        {
+            combat.enabled = !combat.enabled;
+            bow.enabled = !bow.enabled;
+
+            playerMovement.AnimatorSM(PlayerState.Idle);
+            playerMovement.animator.SetBool("isAttacking", false);
+            playerMovement.animator.SetBool("isShooting", false);
+            playerMovement.SetCanBeInterrupted(true);
+            playerMovement.ResetTimer();
+
+            shiftTimer = shiftCooldown;
         }
     }
 }
