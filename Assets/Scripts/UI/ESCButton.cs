e139c5ab7b7899055b8d1141689cae963078ca23
KAIDOYONAGI <1224855487@qq.com>
2026-05-02 22:36:18 +0800

    ESCButton

---
+++ Assets/Scripts/UI/ESCButton.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ESCButton : MonoBehaviour
+{
+    public Button escButton;
+    private void OnEnable() {
+        escButton.onClick.AddListener(OnESC);
+    }
+    private void OnDisable()
+    {
+        escButton.onClick.RemoveAllListeners();
+    }
+    private void OnESC()
+    {
+        UIManager.instance.SetInput(MyEnums.CanvasToToggle.ESC,true);
+    }
+}
