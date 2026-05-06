a19c9a50d5c3e411876e1fe292899e5340a14188
KAIDOYONAGI <1224855487@qq.com>
2026-05-06 23:29:41 +0800

    refactor

--- Assets/Scripts/UI/Shop/ShopPortraitCameraController.cs
+++ Assets/Scripts/UI/Shop/ShopPortraitCameraController.cs
@@ -1,13 +1,8 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 [DisallowMultipleComponent]
 public class ShopPortraitCameraController : MonoBehaviour
 {
-    [Header("Events")]
-    [SerializeField] private ShopLoadEventSO shopLoadEvent;
-    [SerializeField] private ToggleCanvasEventSO toggleShopCanvasEvent;
-
     [SerializeField] private Vector3 followOffset = new(0f, 0f, -2f);
     [SerializeField] private bool hideWhenNoShopKeeper = true;
 
@@ -22,70 +17,35 @@ public class ShopPortraitCameraController : MonoBehaviour
 
     private void OnEnable()
     {
-        if (shopLoadEvent != null)
-        {
-            shopLoadEvent.ShopLoadEvent += OnShopLoaded;
-        }
-
-        if (toggleShopCanvasEvent != null)
-        {
-            toggleShopCanvasEvent.toggleCanvasEvent += OnShopToggle;
-        }
+        SetCameraState(false);
     }
 
     private void OnDisable()
     {
-        if (shopLoadEvent != null)
-        {
-            shopLoadEvent.ShopLoadEvent -= OnShopLoaded;
-        }
-
-        if (toggleShopCanvasEvent != null)
-        {
-            toggleShopCanvasEvent.toggleCanvasEvent -= OnShopToggle;
-        }
-
         currentTarget = null;
         SetCameraState(false);
     }
 
     private void LateUpdate()
     {
-        if (currentTarget == null)
+        if (ShopManager.instance == null) return;
+
+        Transform target = ShopManager.instance.CurrentPortraitTarget;
+        if (target != currentTarget)
         {
-            return;
+            currentTarget = target;
+            SetCameraState(currentTarget != null);
         }
 
-        transform.position = currentTarget.position + followOffset;
-    }
-
-    private void OnShopLoaded(
-        List<ShopItems> shopItems,
-        List<ShopItems> shopWeapon,
-        List<ShopItems> shopArmor,
-        Transform portraitTarget)
-    {
-        currentTarget = portraitTarget;
-        SetCameraState(currentTarget != null);
-    }
-
-    private void OnShopToggle(bool state)
-    {
-        if (state)
+        if (currentTarget != null)
         {
-            return;
+            transform.position = currentTarget.position + followOffset;
         }
-
-        currentTarget = null;
-        SetCameraState(false);
     }
 
     private void SetCameraState(bool state)
     {
-        if (cachedCamera == null || !hideWhenNoShopKeeper)
-        {
-            return;
-        }
+        if (cachedCamera == null || !hideWhenNoShopKeeper) return;
 
         cachedCamera.enabled = state;
     }
