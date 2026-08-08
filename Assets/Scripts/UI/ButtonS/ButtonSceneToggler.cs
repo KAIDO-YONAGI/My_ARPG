d8f1cc2427ddb9ffa060fca6829a508a9fd157d4
KAIDOYONAGI <1224855487@qq.com>
2026-08-09 02:01:07 +0800

    重构切换按钮

--- Assets/Scripts/UI/ButtonS/ButtonSceneToggler.cs
+++ Assets/Scripts/UI/ButtonS/ButtonSceneToggler.cs
@@ -1,7 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using MyEnums;
+
 public class ButtonSceneToggler : MonoBehaviour
 {
     [SerializeField] private SceneLoadEventSO loadEventSO;
@@ -9,23 +7,13 @@ public class ButtonSceneToggler : MonoBehaviour
     [SerializeField] private CanvasGroup ButtonCanvas;
     [SerializeField] private Vector3 newPosition;
     [SerializeField] private bool isToFade = true;
-    [Header("Retry Event")]
-    [SerializeField] private VoidEventSO retryEventSO;
+
     public void HandleSceneToggle()//editor内由button组件绑定
     {
         ButtonCanvas.alpha = 0;
         ButtonCanvas.interactable = false;
         ButtonCanvas.blocksRaycasts = false;
 
-        if (sceneToLoad.sceneType != MyEnums.SceneType.Retry)
-        {
-            loadEventSO.RaiseLoadRequestEvent(sceneToLoad, newPosition, isToFade);
-        }
-        else
-        {
-            TimeManager.instance.ForceResumeGame();
-            retryEventSO.OnEventRaised();//事件引用位于每个场景内RetryManager
-        }
+        loadEventSO.RaiseLoadRequestEvent(sceneToLoad, newPosition, isToFade);
     }
-
 }
